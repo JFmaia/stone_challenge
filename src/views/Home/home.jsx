b87049c5365aeb2c7e72c5d78e073f49398ea1b0
JFmaia <jfmaia741@gmail.com>
2023-06-16 16:02:50 -0300

    feat: Struct base of home

---
+++ src/views/Home/home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export function Home(){
+    return (
+        <div>
+            <header>
+                <nav>
+                    <h1>Stone</h1>
+                </nav>
+            </header>
+            <main>
+                
+            </main>
+        </div> 
+    )
+}
