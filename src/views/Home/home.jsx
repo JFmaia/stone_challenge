5e743f89b20fb283c49068773da64824ccf8868d
JFmaia <jfmaia741@gmail.com>
2023-06-16 18:00:07 -0300

    feat: Modified struct home.jsx

--- src/views/Home/home.jsx
+++ src/views/Home/home.jsx
@@ -1,9 +1,23 @@
+import { ReactComponent as StoneLogo } from '../../assets/stone.svg';
+
 export function Home(){
     return (
         <div>
             <header>
-                <nav>
-                    <h1>Stone</h1>
+                <nav className='container nav-bar'>
+                    <div className='options-about'>
+                        <StoneLogo/>
+                        <a className='item-option' href="">Produtos</a>
+                        <a className='item-option' href="">Sobre a Stone</a>
+                        <a className='item-option' href="">Blog</a>
+                    </div>
+                    <div className='options-contact'>
+                        <a className='item-option' href="">Atendimento</a>
+                        <a className='item-option' href="">Baixe o app</a>
+                        <div className='box-buttons'>
+
+                        </div>
+                    </div>
                 </nav>
             </header>
             <main>
