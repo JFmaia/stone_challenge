7cfb9d4329d07cace6c3d5125ead8c6f9657d289
JFmaia <jfmaia741@gmail.com>
2023-06-17 09:32:45 -0300

    feat: Add news components em struct

--- src/views/Home/home.jsx
+++ src/views/Home/home.jsx
@@ -1,4 +1,13 @@
+// Importando svg como component, logo da Stone
 import { ReactComponent as StoneLogo } from '../../assets/stone.svg';
+
+//Import button light
+import {ButtonLight} from '../../components/ButtonLight/ButtonLight';
+import "../../components/ButtonLight/ButtonLight.css";
+
+//Import button Dark
+import {ButtonDark} from '../../components/ButtonDark/ButtonDark';
+import "../../components/ButtonDark/ButtonDark.css";
 
 export function Home(){
     return (
@@ -15,13 +24,14 @@ export function Home(){
                         <a className='item-option' href="">Atendimento</a>
                         <a className='item-option' href="">Baixe o app</a>
                         <div className='box-buttons'>
-
+                            <ButtonLight>Login</ButtonLight>
+                            <ButtonDark>Seja Stone</ButtonDark>
                         </div>
                     </div>
                 </nav>
             </header>
             <main>
-
+                
             </main>
         </div> 
     )
