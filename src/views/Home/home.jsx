2cb2c4d3c0e905b29198d053a263aea9b809db18
JFmaia <jfmaia741@gmail.com>
2023-06-17 10:18:23 -0300

    feat: Layout finish sectio hero

--- src/views/Home/home.jsx
+++ src/views/Home/home.jsx
@@ -25,13 +25,29 @@ export function Home(){
                         <a className='item-option' href="">Baixe o app</a>
                         <div className='box-buttons'>
                             <ButtonLight>Login</ButtonLight>
-                            <ButtonDark>Seja Stone</ButtonDark>
+                            <ButtonDark className="">Seja Stone</ButtonDark>
                         </div>
                     </div>
                 </nav>
             </header>
             <main>
-                
+                <section className='section-hero'>
+                    <div className='container hero'>
+                        <div className='hero-content'>
+                            <article className='hero-text'>
+                                <h1>
+                                    A parceria de mais de 1 milhão de lojistas brasileiros
+                                </h1>
+                                <h2>
+                                    Ser Stone é juntar soluções de venda com uma conta PJ gratuita, seguros, plano de saúde e atendimento humano em 5 segundos
+                                </h2>
+                            </article>
+                            <ButtonDark className="spanded">
+                                Seja Stone agora
+                            </ButtonDark>
+                        </div>
+                    </div>
+                </section>
             </main>
         </div> 
     )
