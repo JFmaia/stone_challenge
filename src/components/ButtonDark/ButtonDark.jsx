34f061a6a25a38738c2ac71b984805a3368c55c3
JFmaia <jfmaia741@gmail.com>
2023-06-17 10:17:36 -0300

    style: receiving a className

--- src/components/ButtonDark/ButtonDark.jsx
+++ src/components/ButtonDark/ButtonDark.jsx
@@ -3,9 +3,10 @@ import PropTypes from 'prop-types';
 
 export class ButtonDark extends React.Component {
     render() {
-      return <button className="button-dark">{this.props.children}</button>
+      return <button className={this.props.className === "" ? 'button-dark': this.props.className}>{this.props.children}</button>
     }
 }
 ButtonDark.propTypes = {
-    children: PropTypes.string.isRequired
+    children: PropTypes.string.isRequired,
+    className: PropTypes.string.isRequired
 }
