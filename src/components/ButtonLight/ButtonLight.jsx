d2cd4bb8c2c8a9324d19be95ee01f3f5c8225d9d
JFmaia <jfmaia741@gmail.com>
2023-06-22 15:02:03 -0300

    feat: Modified button-ligth

--- src/components/ButtonLight/ButtonLight.jsx
+++ src/components/ButtonLight/ButtonLight.jsx
@@ -3,10 +3,11 @@ import PropTypes from 'prop-types';
 
 export class ButtonLight extends React.Component {
     render() {
-      return <button className="button-light">{this.props.children}</button>
+      return <button className={this.props.className === "" ? 'button-light': this.props.className}>{this.props.children}</button>
     }
 }
 ButtonLight.propTypes = {
-    children: PropTypes.string.isRequired
+    children: PropTypes.string.isRequired,
+    className: PropTypes.string.isRequired
 }
 
